a6b0a46c0753a1101d405cbde55b2023d099e8d2
Albert <albert.bagil@gmail.com>
2021-09-27 18:40:16 +0700

    Tugas 26 Javascript
    Tugas 26 Javascript untuk dikumpulkan ke Niomix

---
+++ Tugas26.js
@@ -0,0 +1,13 @@
+function infinity() {
+  var angka = [2, 39, 76, 50, 9, 7, 41, 2, 24, 1, 16];
+  for (var i = 0; i < angka.length; i++) {
+    var j = isFinite(angka[i]);
+    if (j == true) {
+      console.log("Angka", angka[i], "NOT infinity");
+    }else {
+    console.log("Angka", angka[i], "infinity");  
+    }
+  }
+}
+
+infinity()
